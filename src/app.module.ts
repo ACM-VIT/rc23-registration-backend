7e71bed6d2de819ce03975d32fba9064521ea751
Kaushal Rathi <kaushalrathi24@gmail.com>
2023-02-21 21:59:15 +0530

    fix: docker-compose version changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, CacheModule, HttpException } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TeamsModule } from './teams/teams.module';
@@ -6,10 +6,9 @@ import { ParticipantsModule } from './participants/participants.module';
 import { ConfigModule } from '@nestjs/config';
 import { AdminModule } from './admin/admin.module';
 import configuration from '../config/configuration';
-import { redisConfig, typeOrmConfig, sentry_dsn } from 'config/db.config';
+import { redisConfig, typeOrmConfig } from 'config/db.config';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { SentryModule, SentryInterceptor } from '@ntegral/nestjs-sentry';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -26,12 +25,6 @@ import { SentryModule, SentryInterceptor } from '@ntegral/nestjs-sentry';
       load: [configuration],
       isGlobal: true,
     }),
-    // SentryModule.forRoot({
-    //   dsn: sentry_dsn,
-    //   debug: true,
-    //   environment: 'production',
-    //   logLevels: ['debug'],
-    // }),
     AdminModule,
   ],
   providers: [
@@ -39,18 +32,6 @@ import { SentryModule, SentryInterceptor } from '@ntegral/nestjs-sentry';
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
     },
-    // {
-    //   provide: APP_INTERCEPTOR,
-    //   useFactory: () =>
-    //     new SentryInterceptor({
-    //       filters: [
-    //         {
-    //           type: HttpException,
-    //           //filter: (exception: HttpException) => 500 > exception.getStatus(), // Only report 500 errors
-    //         },
-    //       ],
-    //     }),
-    // },
   ],
 })
 export class AppModule {}
